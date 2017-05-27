90c5a49c2ad14035170fbc4d27d20940f9dc7f55
DavidLee999 <MoonLee003@outlook.com>
2017-05-27 20:47:56 +0200

    definition

--- Ch1/Ch1_Ex13_Collection.cpp
+++ Ch1/Ch1_Ex13_Collection.cpp
@@ -2,10 +2,20 @@
 #include <initializer_list>
 using namespace std;
 
+template <typename Type>
+class Collection{
+private:
+	size_t length;
+	Type* array;
+	
+public:
+	Collection() : length { 0 }, array { nullptr } {};
+	
+	size_t size() { return length; }
+};
 int main()
 {
-	initializer_list<int> a {1,2,3,4};
-	
-	cout << a.size();
+	Collection<int> c {};
+	cout << c.size();
 	return 0;
 }
