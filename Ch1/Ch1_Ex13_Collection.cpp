e32770d6e31c88d0b8234fe24a4fcd880e99040e
DavidLee999 <MoonLee003@outlook.com>
2017-05-27 21:16:34 +0200

    deve

--- Ch1/Ch1_Ex13_Collection.cpp
+++ Ch1/Ch1_Ex13_Collection.cpp
@@ -25,11 +25,22 @@ public:
 		array = new Type[len]{};
 	}
 	
+	Collection(initializer_list<Type> ini_list)
+	{
+		length = ini_list.size();
+		
+		array = new Type[length] {};
+		
+		int counter { 0 };
+		for ( auto i:ini_list) //(initializer_list<Type>::interator ini_iter = ini_list.begin(); ini_iter != ini_list.end; ++ini_iter)
+			array[counter++] = i;
+	}
+	
 	size_t size() { return length; }
 };
 int main()
 {
-	Collection<int> c {-1};
+	Collection<int> c {1,2,3};
 	cout << c.size();
 	return 0;
 }
