06e6c430ad2b1a873edb6e7a1864848aeb794d75
DavidLee999 <MoonLee003@outlook.com>
2017-06-13 21:01:29 +0200

    maxSubSum

--- Ch2/maxSubSum.cpp
+++ Ch2/maxSubSum.cpp
@@ -3,7 +3,7 @@
 #include <ctime>
 using namespace std;
 
-int maxSubSum1( const vector<int> a )
+int maxSubSum1( const vector<int> & a )
 {
 	int maxSum = 0;
 	
@@ -22,17 +22,37 @@ int maxSubSum1( const vector<int> a )
 	return maxSum;
 }
 
+int maxSubSum2( const vector<int> & a )
+{
+	int maxSum = 0;
+	
+	for( int i = 0; i < a.size(); ++i )
+	{
+		int thisSum = 0;
+		for ( int j = i; j < a.size(); ++j )
+		{
+			thisSum += a[ j ];
+			
+			if ( thisSum > maxSum )
+				maxSum = thisSum;
+		}
+	}
+	
+	return maxSum;
+}
+
+
 int main()
 {
 	vector<int> arr { -2, 11, -4, 13, -5, -2 };
 	
-	clock_t begin = clock();
+	//clock_t begin = clock();
 	
-	int max1 = maxSubSum1( arr );
+	int max1 = maxSubSum2( arr );
 	
-	clock_t end = clock();
+	//clock_t end = clock();
 	
-	clock_t spentTime = end - begin;
+	//clock_t spentTime = end - begin;
 	
-	cout << max1 << "\t" << spentTime << "msce.\n";
+	cout << max1 << endl; //"\t" << spentTime << "msce.\n";
 }
