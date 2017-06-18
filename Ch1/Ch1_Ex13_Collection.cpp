414e9d019aa3d00ff855c74e6f33a45f31e758f3
DavidLee999 <MoonLee003@outlook.com>
2017-06-18 12:39:59 +0200

    Collection
    update

--- Ch1/Ch1_Ex13_Collection.cpp
+++ Ch1/Ch1_Ex13_Collection.cpp
@@ -2,11 +2,8 @@
 #include <ostream>
 #include <initializer_list>
 #include <memory>
+#include <algorithm>
 using namespace std;
-
-//template <typename Type> class Collection;
-
-//template <typename Tpye> ostream & operator << (ostream &, const Collection<Tpye> &);
 
 template <typename Type>
 class Collection{
@@ -18,19 +15,19 @@ private:
 	
 	bool enlarge(size_t new_capac)  //increasing capacity
 	{
-		if (new_capac <= length || new_capac <= capacity)
+		if ( new_capac <= length || new_capac <= capacity )
 			return false;
 		
 		Type* new_arr = new Type[new_capac];
 		
 		capacity = new_capac;
 		
-		for( size_t i = 0; i < length; ++i)
+		for( size_t i = 0; i < length; ++i ) 
 		{
 			new_arr[i] = array[i];
 		}
 		
-		swap(array, new_arr);		
+		swap( array, new_arr );		
 		
 		delete[] new_arr;
 		
@@ -38,218 +35,232 @@ private:
 	}
 	
 public:
-	Collection() : length { 0 }, array { nullptr } , capacity { 10 } {} //default constructor
+	Collection() : length { 0 }, array { nullptr } , capacity { 10 } {} // default constructor
 	
-	Collection( int len ) // constructor
-	{
-		if (len <= 0)
-		{
-			length = 0;
-			array = nullptr;
-			cout<<"wrong input."<<endl;
-			return;
-		}
+	Collection( int ); // constructor
+	
+	Collection( const initializer_list<Type> &  ); // constructor
+	
+	Collection ( const Collection & ); // copy constructor
+	
+	Collection ( Collection && ); // move constructor
+	
+	Collection ( Type* ini_arr, Type* end_arr ); // array constructor
+	
+	Collection & operator = ( const Collection & ); // copy assignment
+	
+	Collection & operator = ( Collection && ); //move assignment
+	
+	~Collection() { delete[] array; }
+	
+	Type & at( int ) const; //element accessor
+	
+	Type & operator[] ( int i ) const { return at(i); }	
 		
-		length = len;
-		capacity = 2*length + 1;
-		array = new Type[capacity]{};
+	size_t size() const { return length; }
+	
+	bool isEmpty() const { return ( length == 0 ); }
+	
+	void makeEmpty(); // to be done
+	
+	bool insert( int , const Type & );
+	
+	bool remove( int );
+	
+	size_t getCapacity() const { return capacity; }
+	
+	bool contain(const Type &) const;
+	
+};
+
+template <typename Type>
+Collection<Type>::Collection( int len ) // constructor
+{
+	if (len <= 0)
+	{
+		length = 0;
+		array = nullptr;
+		cout << "wrong input." << endl;
+		return;
 	}
-	
-	Collection(const initializer_list<Type> & ini_list) : length { ini_list.size() }// constructor
-	{
-		//length = ini_list.size();
 		
-		capacity = 2*length + 1;
+	length = len;
+	capacity = 2*length + 1;
+	array = new Type[capacity]{};
+}
+
+template <typename Type>	
+Collection<Type>::Collection(const initializer_list<Type> & ini_list) : length { ini_list.size() }// constructor
+{		
+	capacity = 2*length + 1;
 		
-		array = new Type[capacity] {};
+	array = new Type[capacity] {};
 		
 		/* int counter { 0 };
 		for ( auto i:ini_list) //(initializer_list<Type>::interator ini_iter = ini_list.begin(); ini_iter != ini_list.end; ++ini_iter)
 			array[counter++] = i; */
 		
-		uninitialized_copy( ini_list.begin(), ini_list.end(), array );
-	}
-	
-	Collection ( const Collection &rhs ):length{ rhs.size() }, capacity { rhs.getCapacity() } //copy constructor
-	{	
-		array = new Type[capacity] {};
+	uninitialized_copy( ini_list.begin(), ini_list.end(), array );
+}
+template <typename Type>
+Collection<Type>::Collection ( const Collection &rhs ):length{ rhs.size() }, capacity { rhs.getCapacity() } //copy constructor
+{	
+	array = new Type[capacity] {};
 		
 		/* for ( int i = 0; i < length; ++i)
 			array[i] = rhs[i]; */
-		uninitialized_copy( &rhs[0], &rhs[0] + rhs.size(), array );
-	}
-	
-	Collection ( Collection &&rhs ) : length { rhs.size() }, capacity { rhs.getCapacity() }, array { rhs.array }// move constructor
-	{
-		//length =  rhs.size();
 		
-		//capacity = 2*length + 1;
-		
-		//array = new Type[capacity] {};		
-		rhs.length = 0;
-		rhs.array = nullptr;
-		//swap(array, rhs.array);
-	}
+	uninitialized_copy( &rhs[0], &rhs[0] + rhs.size(), array );
+}
 	
-	Collection (Type* ini_arr, Type* end_arr) //array constructor
-	{
-		length = end_arr - ini_arr;
-		
-		//length = sizeof(ini_arr) / sizeof(Type);
-		
-		capacity = 2*length + 1;
-		
-		array = new Type[capacity]{};
-		
-		for ( size_t i = 0; i < length; ++i)
-			array[i] = ini_arr[i];
-	}
-	
-	
-	~Collection() { delete[] array; }
-	
-	Type & at( int i ) const //element accessor
-	{
-		if ( i < 0 || i >= length )
-		{
-			cout << "wrong index." << endl;
-			exit(0);
-		}
-		
-		return array[i];
-	}
-	
-	Type & operator[] ( int i ) const { return at(i); }	
-	
-	Collection & operator = (const Collection &rhs) //copy assignment
-	{
-		if ( this !=  &rhs )
-		{
-			length = rhs.size();
-			
-			capacity = 2*length + 1;
-			
-			array = new Type[capacity] {};
-			
-			for (int i = 0; i < length; ++i)
-				array[i] = rhs[i]; 
-			
-		}
-		
-		return *this;
-	}
-	
-	Collection & operator = ( Collection &&rhs ) //move assignment
+template <typename Type>
+Collection<Type>::Collection ( Collection &&rhs ) : length { rhs.size() }, capacity { rhs.getCapacity() }, array { rhs.array }// move constructor
+{	
+	rhs.length = 0;
+	rhs.array = nullptr;
+}
+
+template <typename Type> 
+Collection<Type> & Collection<Type>::operator = (const Collection &rhs) //copy assignment
+{
+	if ( this !=  &rhs )
 	{
 		length = rhs.size();
-		
-		capacity = rhs.getCapacity();
-		
-		swap( array, rhs.array );
-		
-		return *this;
-	}
-	
-	size_t size() const { return length; }
-	
-	bool isEmpty() const { return (length == 0); }
-	
-	void makeEmpty()// to be done
-	{
-		for(size_t i = 0; i < length; ++i)
-			array[i] = 0;
-		
- 		length = 0;
-		//delete[] array;
-		//array = nullptr;
-	}
-	
-	bool insert( int index, const Type &t)
-	{
-		if ( index < 0 || index > length )
-		{
-			cout << "wrong index." <<endl;
-			return false;
-		}
-		
-		if ( index == 0 && length == 0)
-		{
-			++length;
-			capacity = 2*length + 1;
-			array = new Type[capacity] {};
 			
-			array[index] = t;
-		}
-		
-		if ( length == capacity )
-		{
-			bool su = enlarge(2*capacity);
+		capacity = 2*length + 1;
 			
-			if (!su) return false;
-		}
-		
-		for ( size_t i = length-1; i >= index; --i )
-		{
-			array[i+1] = array[i];
-		}
-		
-		array[index] = t;
-		
-		length++;
-		
-		return true;
-		
+		array = new Type[capacity] {};
+			
+		for ( int i = 0; i < length; ++i )
+			array[i] = rhs[i]; 
+			
 	}
+		
+	return *this;
+}
 	
-	bool remove(int index)
+template <typename Type>
+Collection<Type> & Collection<Type>::operator = ( Collection &&rhs ) //move assignment
+{
+	length = rhs.size();
+		
+	capacity = rhs.getCapacity();
+		
+	swap( array, rhs.array );
+		
+	return *this;
+}	
+
+template <typename Type>
+Collection<Type>::Collection ( Type* ini_arr, Type* end_arr ) //array constructor
+{
+	length = end_arr - ini_arr;
+		
+	capacity = 2*length + 1;
+		
+	array = new Type[capacity] {};
+		
+	for ( size_t i = 0; i < length; ++i )
+		array[i] = ini_arr[i];
+}
+	
+
+template <typename Type> 
+Type & Collection<Type>::at( int i ) const //element accessor
+{
+	if ( i < 0 || i >= length )
 	{
-		if ( index < 0 || index >= length )
-		{
-			cout << "wrong index." <<endl;
-			return false;
-		}
-		
-		if( index == 0 && length == 1)
-		{
-			makeEmpty();
-			return true;
-		}
-		
-		for ( size_t i = index; i < length - 1; ++i)
-		{
-			array[i] = array[i + 1];
-		}
-		
-		--length;
-		
-		return true;
-		
+		cout << "wrong index." << endl;
+		exit(0);
 	}
-	
-	/* void print()
-	{
-		for ( size_t i = 0; i < length; ++i)
-			cout << array[i] << " ";
 		
-		cout<<endl;
-	} */
+	return array[i];
+}
 	
-	size_t getCapacity() const { return capacity; }
-	
-	bool contain(const Type &t) const
-	{
-		for ( size_t i = 0; i < length; ++i)
-		{
-			if (array[i] == t)
-				return true;
-		}
+template <typename Type>
+void Collection<Type>::makeEmpty()// to be done
+{
+	for(size_t i = 0; i < length; ++i)
+		array[i] = 0;
 		
+ 	length = 0;
+}
+	
+template <typename Type>
+bool Collection<Type>::insert( int index, const Type &t)
+{
+	if ( index < 0 || index > length )
+	{
+		cout << "wrong index." << endl;
 		return false;
 	}
-	//template <typename Tpye> 
-	//friend ostream & operator << (ostream &, const Collection<Type> & );
+		
+	if ( index == 0 && length == 0)
+	{
+		++length;
+		capacity = 2*length + 1;
+		array = new Type[capacity] {};
+		
+		array[index] = t;
+	}
 	
-};
+	if ( length == capacity )
+	{
+		bool su = enlarge(2*capacity);
+		
+		if (!su) return false;
+	}
+	
+	for ( size_t i = length-1; i >= index; --i )
+	{
+		array[i+1] = array[i];
+	}
+	
+	array[index] = t;
+	
+	++length;
+	
+	return true;
+	
+}
+
+template <typename Type>
+bool Collection<Type>::remove(int index)
+{
+	if ( index < 0 || index >= length )
+	{
+		cout << "wrong index." << endl;
+		return false;
+	}
+	
+	if( index == 0 && length == 1)
+	{
+		makeEmpty();
+		return true;
+	}
+	
+	for ( size_t i = index; i < length - 1; ++i)
+	{
+		array[i] = array[i + 1];
+	}
+	
+	--length;
+	
+	return true;
+	
+}
+
+template <typename Type>
+bool Collection<Type>::contain(const Type &t) const
+{
+	for ( size_t i = 0; i < length; ++i)
+	{
+		if (array[i] == t)
+			return true;
+	}
+	
+	return false;
+}
 
 template <typename Type>
 ostream & operator << (ostream &out, const Collection<Type> &obj)
@@ -267,69 +278,11 @@ ostream & operator << (ostream &out, const Collection<Type> &obj)
 
 
 int main()
-{
-	Collection<int> dd {};
-	
-	dd = Collection<int> {1,2,3};
-	
-	cout<<"dd: " << dd;
-	
+{	
 	Collection<int> c {1, 2, 3};
+	c.insert(3, 4);
 	
-	Collection<int> a {c};
+	cout << c << c.size();
 	
-	c.insert(1,0);
-	
-	c.insert(c.size(),4);
-	
-	c.insert(c.size(),5);
-	
-	c.insert(c.size(),6);
-	
-	c.insert(c.size(),7);
-	
-	//c.print();
-	
-	cout << c;
-	
-	cout<<c.size()<<endl<<c.getCapacity()<<endl;
-	
-	c.remove(3);
-	
-	cout<<c.size()<<endl;
-	
-	cout << c;
-	
-	a.remove(0);
-	
-	a.remove(0);
-	
-	a.remove(0);
-	
-	cout<< a.size()<<endl;
-	
-	cout<<c.contain(4)<<" "<<c.contain(100)<<endl;
-	
-	Collection<int> b {};
-    b = c;
-	
-	cout << "b" << b;
-	
-	int d[4] {4,3,2,1};
-	
-	Collection<int> e{&d[0], &d[4]}; //one beyond the array's length is required
-	
-	cout << e;
-
-	cout<<e.size()<<endl<<e.getCapacity()<<endl;
-	
-	e.makeEmpty();
-	
-	cout << "e: " << e;
-	cout<<e.size()<<endl<<e.getCapacity()<<endl;
-	cout <<"c: " << c;
-	Collection<int>aa = move(c);
-	cout << "aa: " << aa;
-	cout << "c: " << c;
 	return 0;
 }
