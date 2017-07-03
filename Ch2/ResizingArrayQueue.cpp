05c9cee50f83d863c53bbf21a1f8ab75eba36309
PenghuaLI <MoonLee003@outlook.com>
2017-07-03 12:07:48 +0200

    ResizingArrayQueue

---
+++ Ch2/ResizingArrayQueue.cpp
@@ -0,0 +1,88 @@
+template <typename Type>
+class Queue{
+	private:
+	    int head;
+		int last;
+		int theSize;
+		int theCapacity;
+		static const int SPARE_CAPACITY = 2;
+		Type* arr;
+		
+	public:
+	    Queue() : theSize{ 0 }, theCapacity{ theSize + SPARE_CAPACITY }, head{ 0 }, last{ 0 }
+		{
+			arr = new Type[theCapacity]{};
+		}
+		
+		Queue( const Queue& rhs ) : theSize{ rhs.theSize }, theCapacity{ rhs.theCapacity }
+		{
+		    arr = new Type[theCapacity]{};
+			for( int i = 0; i < rhs.theSize; ++i )
+				arr[0] = rhs.arr[(rhs.head + i) % rhs.theCapacity]
+			
+			head = 0;
+			last = rhs.theSize;
+		}
+		
+		Queue§ operator = ( const Queue& rhs )
+		{
+			Queue copy = rhs;
+			std::swap( *this, copy );
+			
+			return *this;
+		}
+		
+		Queue( Queue&& rhs ) : theSize{ rhs.theSize }, theCapacity{ rhs.theCapacity }, head{ rhs.head }, last{ rhs.last }, arr{ rhs.arr }
+		{
+			rhs.theSize = 0;
+			rhs.theCapacity = 0;
+			rhs.head = 0;
+			rhs.last = 0;
+			rhs.arr = nullptr;
+		}
+		
+		Queue& operator = ( Queue&& rhs )
+		{
+			std::swap( theSize, rhs.theSize );
+            std::swap( theCapacity, rhs.theCapacity );
+            std::swap( head, rhs.head );
+            std::swap( last, rhs.last );
+            std::swap( arr, rhs.arr );
+			
+			return *this;
+		}
+		
+		~Queue() { delete[] arr; }
+		
+		void enqueue( const Type& x )
+		{
+			if( theSize == theCapacity )
+				reserve( 2 * theCapacity );
+			
+			arr[last++] = x;
+			theSize++;
+			
+			if( last == theCapacity )
+				last = 0;
+		}
+		
+		Type dequeue()
+		{
+			Type item = arr[head];
+			
+			arr[head++] = Type{};
+			theSize--;
+			
+			if( head == theCapacity )
+				head = 0;
+			
+			if( theSize > 0 && theSize == theCapacity / 4 )
+				reserve( theCapacity / 2 );
+			
+			return item;
+		}
+		
+		bool isEmpty() const { return theSize == 0; }
+		int size() const { return theSize; }
+		
+};
