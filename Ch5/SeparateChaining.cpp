8245fd3b8953ea19ec722ec26f7b432f17e4a70b
DavidLee999 <MoonLee003@outlook.com>
2017-08-30 19:43:02 +0200

    separate chaining

--- Ch5/SeparateChaining.cpp
+++ Ch5/SeparateChaining.cpp
@@ -3,84 +3,172 @@
 #include <list>
 #include <string>
 #include <algorithm>
-#include <functional>
 using namespace std;
 
-template <>
-class hash<string>;
-
 class Employee;
+size_t hash(const string&);
+size_t hash(const Employee&);
+int isPrime(int);
+int nextPrime(int);
 
-template <>
-class hash<Employee>;
 template <typename HashedObj>
 class HashTable
 {
     public:
-        explicit HashTable(int size = 101);
+        explicit HashTable(int size = 101) : currentSize{ 0 }
+        { theLists.resize(size); }
 
-        bool contains(const HashedObj& x) const;
+        bool contains(const HashedObj& x) const
+        {
+            auto& whichList = theLists[myhash(x)];
+            return find(begin(whichList), end(whichList), x) != end(whichList);
+        }
 
-        void makeEmpty();
-        bool insert(const HashedObj& x);
-        bool insert(HashedObj&& x);
-        bool remove(const HashedObj& x);
+        void makeEmpty()
+        {
+            for (auto& thisList : theLists)
+                thisList.clear();
+        }
+
+        bool insert(const HashedObj& x)
+        {
+            auto& whichList = theLists[myhash(x)];
+            if (find(begin(whichList), end(whichList), x) != end(whichList))
+                return false;
+
+            whichList.push_back(x);
+
+            if (++currentSize > theLists.size())
+                rehash();
+
+            return true;
+        }
+        
+        bool insert(HashedObj&& x)
+        {
+            auto& whichList = theLists[myhash(x)];
+            if (find(begin(whichList), end(whichList), x) != end(whichList))
+                return false;
+
+            whichList.push_back(std::move(x));
+
+            if (++currentSize > theLists.size())
+                rehash();
+
+            return true;
+        }
+
+        bool remove(const HashedObj& x)
+        {
+            auto& whichList = theLists[myhash(x)];
+            auto itr = find(begin(whichList), end(whichList), x);
+
+            if (itr == end(whichList))
+                return false;
+
+            whichList.erase(itr);
+            --currentSize;
+
+            return true;
+        }
 
     private:
         vector< list<HashedObj> > theLists;
         int currentSize;
 
-        void rehash();
-        size_t myhash(const HashedObj& x) const;
+        void rehash()
+        {
+            vector< list<HashedObj> > oldLists = theLists;
+
+            theLists.resize(nextPrime(2 * theLists.size()));
+
+            for (auto& thisList : theLists)
+                thisList.clear();
+
+            currentSize = 0;
+            
+            for (auto& thisList : oldLists)
+                for (auto& x : thisList)
+                    insert(std::move(x));
+        }
+
+        size_t myhash(const HashedObj& x) const
+        {
+            return ::hash(x) % theLists.size();
+        }
 };
 
 class Employee
 {
     public:
-        explicit Employee(const string& n, const double& d, const int& s)
-            : name{ n }, salary{ d }, seniority{ s } {};
+        explicit Employee(const string& n, const double& d)
+            : name{ n }, salary{ d } {};
 
         const string& getName() const
         { return name; }
 
-        bool operator==(const Employee& rhs) const
+        bool operator== (const Employee& rhs) const
         { return getName() == rhs.getName(); }
-        bool operator!=(const Employee& rhs) const
+        bool operator!= (const Employee& rhs) const
         { return !(*this == rhs); }
 
     private:
         string name;
         double salary;
-        int seniority;
 };
 
-template <>
-class hash<string>
+size_t hash(const string& key)
 {
-    public:
-        size_t operator= (const string& key)
-        {
-            size_t hashVal = 0;
+    size_t hashVal = 0;
 
-            for (char ch : key)
-                hashVal = 37 * hashVal + ch;
+    for (char ch : key)
+        hashVal = 37 * hashVal + ch;
 
-            return hashVal;
-        }
-};
+    return hashVal;
+}
 
-template<>
-class hash<Employee>
+int isPrime(int n)
 {
-    public:
-        size_t operator() (const Employee& item)
-        {
-            static hash<string> hf;
-            return hf(item.getName());
-        }
-};
+    if (n == 2 || n == 3)
+        return true;
+
+    if (n == 1 || n % 2 == 0)
+        return false;
+    
+    for (int i = 3; i * i < n; i += 2)
+        if (n % i == 0)
+            return false;
+
+    return true;
+}
+
+int nextPrime(int n)
+{
+    if (n % 2 == 0)
+        ++n;
+
+    while (!isPrime(n))
+        n += 2;
+
+    return n;
+}
+
+size_t hash(const Employee& e)
+{
+    return ::hash(e.getName());
+}
 
 int main()
 {
+    HashTable<Employee> a;
+    a.insert(Employee{"Mike", 2000});
+    a.insert(Employee{"John", 3000});
+    a.insert(Employee{"David", 4000});
+    a.insert(Employee{"Thomi", 5000});
+    a.insert(Employee{"Felix", 6000});
+
+    a.remove(Employee{"Thomi", 5000});
+
+    cout << a.contains(Employee{"John", 3000});
     return 0;
 }
