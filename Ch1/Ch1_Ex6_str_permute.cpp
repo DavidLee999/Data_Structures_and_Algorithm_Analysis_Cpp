a4bd7c46733a404c084adaa4a0dd515e188c73e1
DavidLee999 <MoonLee003@outlook.com>
2017-08-30 22:05:38 +0200

    update

--- Ch1/Ch1_Ex6_str_permute.cpp
+++ Ch1/Ch1_Ex6_str_permute.cpp
@@ -4,45 +4,45 @@ using namespace std;
 
 bool isSwap(const string str, const int begin, const int end)
 {
-	for ( int i = begin; i < end; ++i)
-	{
-		if (str[i] == str[end])
-			return false;
-	}
-	
-	return true;
+    for ( int i = begin; i < end; ++i)
+    {
+        if (str[i] == str[end])
+            return false;
+    }
+
+    return true;
 }
 void permute (const string str, int low, int high)
 {
-	string strCopy = str;
-	
-	if (low == high )
-		cout << str << endl;
-	else
-	{
-		for ( int i = low; i <= high; ++i)
-		{
-			if(isSwap(strCopy, low, i))
-			{
-				swap(strCopy[low], strCopy[i]);
-				permute(strCopy, low + 1, high);
-				swap(strCopy[low], strCopy[i]);
-			}
-		}
-	}
+    string strCopy = str;
+
+    if (low == high)
+        cout << str << endl;
+    else
+    {
+        for (int i = low; i <= high; ++i)
+        {
+            if(isSwap(strCopy, low, i))
+            {
+                swap(strCopy[low], strCopy[i]);
+                permute(strCopy, low + 1, high);
+                swap(strCopy[low], strCopy[i]);
+            }
+        }
+    }
 }
 
 void permute ( const string str )
 {
-	permute( str, 0, str.size() - 1 );
+    permute( str, 0, str.size() - 1 );
 }
 int main()
 {
-	string str {};
-	
-	cin>>str;
-	
-	cout<<endl;
+    string str {};
 
-	permute(str);
+    cin>>str;
+
+    cout<<endl;
+
+    permute(str);
 }
