09f6d9851b15ce71c17fc63d587798f18db65bb3
DavidLee999 <MoonLee003@outlook.com>
2017-09-13 21:16:06 +0200

    topological sort

--- Ch9/Vertex.h
+++ Ch9/Vertex.h
@@ -3,19 +3,40 @@
 
 #include <iostream>
 #include <List>
+#include <algorithm>
 using namespace std;
 
 // template <typename Name>
 struct Vertex
 {
     int name;
+    int indegree;
     list<Vertex> adj;
 
     Vertex () {}
     
-    Vertex(const int& n) : name { n }
+    Vertex(const int& n) : name { n }, indegree { 0 }, adj { }
     {
     }
+
+    void add(Vertex& t)
+    {
+        if (find(adj.begin(), adj.end(), t) == adj.end())
+        {
+            adj.push_back(t);
+            // ++indegree;
+        }
+    }
+
+    bool isLinked(const Vertex& t) const
+    {
+        auto it = find(adj.begin(), adj.end(), t);
+        if ( it != adj.end())
+            return true;
+        else
+            return false;
+    }
+
 };
 
 bool operator== (const Vertex& p, const Vertex& q)
--- Ch9/topologicalSort.cpp
+++ Ch9/topologicalSort.cpp
@@ -1,48 +1,93 @@
 #include <iostream>
-#include <list>
+// #include <list>
 #include <vector>
+#include <queue>
 #include <algorithm>
-#include <memory>
+// #include <memory>
 using namespace std;
 
 #include "Vertex.h"
 
+void topSort(const vector<Vertex>& points)
+{
+    std::vector<Vertex> p = points;
+    std::queue<Vertex> q;
+    
+    for (auto it = p.begin(); it != p.end(); ++it)
+    {
+        if (it->indegree == 0)
+        {
+            q.push(*it);
+            // p.erase(it);
+        }
+    }
+
+    while (!q.empty()) {
+        Vertex v = q.front();
+        q.pop();
+
+        cout << v.name << " ";
+
+        for (auto it = v.adj.begin(); it != v.adj.end(); ++it)
+        {
+            auto it2 = find(p.begin(), p.end(), *it);
+            if (--(it2->indegree) == 0)
+                q.push(*it2);
+
+            // if (it->isLinked(v))
+            // {
+                // if (--(it->indegree) == 0)
+                    // q.push(*it);
+            // }
+        }
+    }
+
+}
 
 int main()
 {
     vector<Vertex> points;
     int from, to;
 
-    Vertex p1, p2;
-    while (cin) {
-        cin >> from >> to;
-        p1 = Vertex {from};
-        p2 = Vertex {to};
+    // Vertex p1, p2;
+    while (cin >> from >> to) {
+        Vertex p1 = Vertex {from};
+        Vertex p2 = Vertex {to};
 
         auto it = find(points.begin(), points.end(), p1);
         if (it != points.end()) // included
-        {
-            if (find(it->adj.begin(), it->adj.end(), p2) == it->adj.end())
-                it->adj.push_back(p2);
-        }
+            it->add(p2);
+        // {
+            // if (find(it->adj.begin(), it->adj.end(), p2) == it->adj.end())
+                // it->adj.push_back(p2);
+        // }
         else if (it == points.end())
         {
-            p1.adj.push_back(p2);
+            p1.add(p2);
             points.push_back(p1);
         }
 
-        if (find(points.begin(), points.end(), p2) == points.end()) {
+        auto it2 = find(points.begin(), points.end(), p2);
+        if (it2 == points.end())
+        {
+            ++p2.indegree;
             points.push_back(p2);
         }
+        else
+            (it2->indegree)++;
     }
 
     for (auto it = points.begin(); it != points.end(); ++it)
     {
-        cout << it->name << ": ";
+        cout << it->name << " " << it->indegree <<  ": ";
         for (auto it2 = it->adj.begin(); it2 != it->adj.end(); ++it2)
             cout << it2->name << " ";
         cout << endl;
     }
+
+    cout << "topological sort: ";
+    topSort(points);
+    cout << endl;
 
     return 0;
 }
